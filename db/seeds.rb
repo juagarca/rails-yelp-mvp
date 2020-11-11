37fbbbc430df89614e4aefc41aa1511c41acd193
Juan Garcia <juagarca@gmail.com>
2020-11-11 12:14:59 +0000

    validations and db has been seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'start'
+
+Restaurant.create!(name: 'Name1', address: 'address', phone_number: 123_456_789, category: 'chinese')
+Restaurant.create!(name: 'Name2', address: 'address', phone_number: 123_456_789, category: 'italian')
+Restaurant.create!(name: 'Name3', address: 'address', phone_number: 123_456_789, category: 'japanese')
+Restaurant.create!(name: 'Name4', address: 'address', phone_number: 123_456_789, category: 'french')
+Restaurant.create!(name: 'Name5', address: 'address', phone_number: 123_456_789, category: 'belgian')
+
+puts 'end'
