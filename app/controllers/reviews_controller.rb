4b6bbfe8a9914d54744cdde18c4c11afa5ef21ac
Juan Garcia <juagarca@gmail.com>
2020-11-11 15:22:02 +0000

    app finished

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,4 @@
 class ReviewsController < ApplicationController
-  def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Review.new
-  end
-
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
@@ -12,7 +7,7 @@ class ReviewsController < ApplicationController
     if @review.save
       redirect_to restaurant_path(@restaurant.id)
     else
-      render :new
+      render 'restaurants/show'
     end
   end
 
